5d53b0e668b9a84e761b49f692b039d5000f02aa
Andras Barthazi <andras@barthazi.hu>
2022-01-14 16:39:40 +0100

    refactoring the code, adding image upload

--- js/index.js
+++ js/index.js
@@ -9,43 +9,142 @@ const connectButton = document.getElementById('button-connect');
 const echoButton = document.getElementById('button-echo');
 const disconnectButton = document.getElementById('button-disconnect');
 const resetButton = document.getElementById('button-reset');
+const imageStateButton = document.getElementById('button-image-state');
+const eraseButton = document.getElementById('button-erase');
 const imageList = document.getElementById('image-list');
+const fileInfo = document.getElementById('file-info');
+const fileImage = document.getElementById('file-image');
+const fileUpload = document.getElementById('file-upload');
 
-const device = new NewtManager();
-device.onConnecting(() => {
+const mcumgr = new MCUManager();
+mcumgr.onConnecting(() => {
     console.log('Connecting...');
     screens.connecting.style.display = 'block';
     screens.initial.style.display = 'none';
     screens.connected.style.display = 'none';
 });
-device.onConnect(() => {
-    deviceName.innerText = device.deviceName;
+mcumgr.onConnect(() => {
+    deviceName.innerText = mcumgr.name;
     screens.connecting.style.display = 'none';
     screens.initial.style.display = 'none';
     screens.connected.style.display = 'block';
+    imageList.innerHTML = '';
+    mcumgr.cmdImageState();
 });
-device.onDisconnect(() => {
+mcumgr.onDisconnect(() => {
     deviceName.innerText = 'Connect your device';
     screens.connecting.style.display = 'none';
     screens.initial.style.display = 'block';
     screens.connected.style.display = 'none';
 });
-device.onMessage(message => {
+
+mcumgr.onMessage(({ op, group, id, data, length }) => {
+    console.log({ op, group, id, data, length });
+    switch (group) {
+        case MGMT_GROUP_ID_OS:
+            switch (id) {
+                case OS_MGMT_ID_ECHO:
+                    alert(data.r);
+                    break;
+                case OS_MGMT_ID_TASKSTAT:
+                    console.table(data.tasks);
+                    break;
+                case OS_MGMT_ID_MPSTAT:
+                    console.log(data);
+                    break;
+            }
+            break;
+        case MGMT_GROUP_ID_IMAGE:
+            switch (id) {
+                case IMG_MGMT_ID_STATE:
+                    let images = '';
+                    data.images.forEach(image => {
+                        images += `<div class="image ${image.active ? 'active' : 'standby'}">`;
+                        images += `<h2>Slot #${image.slot} ${image.active ? 'active' : 'standby'}</h2>`;
+                        images += '<table>';
+                        const hashStr = Array.from(image.hash).map(byte => byte.toString(16).padStart(2, '0')).join('');
+                        images += `<tr><th>Version</th><td>v${image.version}</td></tr>`;
+                        images += `<tr><th>Bootable</th><td>${image.bootable}</td></tr>`;
+                        images += `<tr><th>Confirmed</th><td>${image.confirmed}</td></tr>`;
+                        images += `<tr><th>Pending</th><td>${image.pending}</td></tr>`;
+                        images += `<tr><th>Hash</th><td>${hashStr}</td></tr>`;
+                        images += '</table>';
+                        images += '</div>';
+                    });
+                    imageList.innerHTML = images;
+                    break;
+            }
+            break;
+        default:
+            console.log('Unknown group');
+            break;
+    }
+});
+
+let file = null;
+let fileData = null;
+let sha256sum = null;
+
+mcumgr.onImageUploadProgress(({ percentage }) => {
+    fileInfo.innerText = `Uploading... ${percentage}%`;
+});
+
+mcumgr.onImageUploadFinished(() => {
+    fileInfo.innerText = 'Upload complete';
+    mcumgr.cmdImageState();
+});
+
+fileImage.addEventListener('change', () => {
+    file = fileImage.files[0];
+    fileData = null;
+    const reader = new FileReader();
+    reader.onload = async () => {
+        fileData = reader.result;
+        try {
+            const info = await mcumgr.imageInfo(fileData);
+            let table = `<table>`
+            table += `<tr><th>Version</th><td>v${info.version}</td></tr>`;
+            table += `<tr><th>Hash</th><td>${info.hash}</td></tr>`;
+            table += `<tr><th>File Size</th><td>${fileData.byteLength} bytes</td></tr>`;
+            table += `<tr><th>Size</th><td>${info.imageSize} bytes</td></tr>`;
+            table += `</table>`;
+
+            fileInfo.innerHTML = table;
+            fileUpload.disabled = false;
+        } catch (e) {
+            fileInfo.innerHTML = `ERROR: ${e.message}`;
+        }
+    };
+    reader.readAsArrayBuffer(file);
+});
+fileUpload.addEventListener('click', () => {
+    if (file && fileData) {
+        mcumgr.cmdUpload(fileData);
+    }
+    return false;
 });
 
 connectButton.addEventListener('click', async () => {
-    await device.connect();
+    await mcumgr.connect();
 });
 
 disconnectButton.addEventListener('click', async () => {
-    device.disconnect();
+    mcumgr.disconnect();
 });
 
 echoButton.addEventListener('click', async () => {
     const message = prompt('Enter a text message to send', 'Hello World!');
-    await device.smpEcho(message);
+    await mcumgr.smpEcho(message);
 });
 
 resetButton.addEventListener('click', async () => {
-    await device.smpReset();
+    await mcumgr.cmdReset();
+});
+
+imageStateButton.addEventListener('click', async () => {
+    await mcumgr.cmdImageState();
+});
+
+eraseButton.addEventListener('click', async () => {
+    await mcumgr.cmdImageErase();
 });
---
+++ js/mcumgr.js
@@ -0,0 +1,268 @@
+
+// Opcodes
+const MGMT_OP_READ = 0;
+const MGMT_OP_READ_RSP = 1;
+const MGMT_OP_WRITE = 2;
+const MGMT_OP_WRITE_RSP = 3;
+
+// Groups
+const MGMT_GROUP_ID_OS = 0;
+const MGMT_GROUP_ID_IMAGE = 1;
+const MGMT_GROUP_ID_STAT = 2;
+const MGMT_GROUP_ID_CONFIG = 3;
+const MGMT_GROUP_ID_LOG = 4;
+const MGMT_GROUP_ID_CRASH = 5;
+const MGMT_GROUP_ID_SPLIT = 6;
+const MGMT_GROUP_ID_RUN = 7;
+const MGMT_GROUP_ID_FS = 8;
+const MGMT_GROUP_ID_SHELL = 9;
+
+// OS group
+const OS_MGMT_ID_ECHO = 0;
+const OS_MGMT_ID_CONS_ECHO_CTRL = 1;
+const OS_MGMT_ID_TASKSTAT = 2;
+const OS_MGMT_ID_MPSTAT = 3;
+const OS_MGMT_ID_DATETIME_STR = 4;
+const OS_MGMT_ID_RESET = 5;
+
+// Image group
+const IMG_MGMT_ID_STATE = 0;
+const IMG_MGMT_ID_UPLOAD = 1;
+const IMG_MGMT_ID_FILE = 2;
+const IMG_MGMT_ID_CORELIST = 3;
+const IMG_MGMT_ID_CORELOAD = 4;
+const IMG_MGMT_ID_ERASE = 5;
+
+class MCUManager {
+    constructor(di = {}) {
+        this.SERVICE_UUID = '8d53dc1d-1db7-4cd3-868b-8a527460aa84';
+        this.CHARACTERISTIC_UUID = 'da2e7828-fbce-4e01-ae9e-261174997c48';
+        this._mtu = 140;
+        this._device = null;
+        this._service = null;
+        this._characteristic = null;
+        this._connectCallback = null;
+        this._connectingCallback = null;
+        this._disconnectCallback = null;
+        this._messageCallback = null;
+        this._imageUploadProgressCallback = null;
+        this._uploadIsInProgress = false;
+        this._buffer = new Uint8Array();
+        this._logger = di.logger || { info: console.log, error: console.error };
+        this._seq = 0;
+    }
+    async _requestDevice() {
+        return navigator.bluetooth.requestDevice({
+            acceptAllDevices: true,
+            optionalServices: [this.SERVICE_UUID]
+        });
+    }
+    async connect() {
+        try {
+            this._device = await this._requestDevice();
+            this._logger.info(`Connecting to device ${this.name}...`);
+            this._device.addEventListener('gattserverdisconnected', async event => {
+                this._logger.info(event);
+                this._disconnected();
+            });
+            if (this._connectingCallback) this._connectingCallback();
+            const server = await this._device.gatt.connect();
+            this._logger.info(`Server connected.`);
+            this._service = await server.getPrimaryService(this.SERVICE_UUID);
+            this._logger.info(`Service connected.`);
+            this._characteristic = await this._service.getCharacteristic(this.CHARACTERISTIC_UUID);
+            this._characteristic.addEventListener('characteristicvaluechanged', this._notification.bind(this));
+            await this._characteristic.startNotifications();
+        } catch (error) {
+            this._logger.error(error);
+            await this._disconnected();
+            return;
+        }
+        await this._connected();
+    }
+    disconnect() {
+        return this._device.gatt.disconnect();
+    }
+    onConnecting(callback) {
+        this._connectingCallback = callback;
+        return this;
+    }
+    onConnect(callback) {
+        this._connectCallback = callback;
+        return this;
+    }
+    onDisconnect(callback) {
+        this._disconnectCallback = callback;
+        return this;
+    }
+    onMessage(callback) {
+        this._messageCallback = callback;
+        return this;
+    }
+    onImageUploadProgress(callback) {
+        this._imageUploadProgressCallback = callback;
+        return this;
+    }
+    onImageUploadFinished(callback) {
+        this._imageUploadFinishedCallback = callback;
+        return this;
+    }
+    async _connected() {
+        if (this._connectCallback) this._connectCallback();
+    }
+    async _disconnected() {
+        this._logger.info('Disconnected.');
+        if (this._disconnectCallback) this._disconnectCallback();
+        this._device = null;
+        this._service = null;
+        this._characteristic = null;
+        this._uploadIsInProgress = false;
+    }
+    get name() {
+        return this._device && this._device.name;
+    }
+    async _sendMessage(op, group, id, data) {
+        const _flags = 0;
+        let encodedData = [];
+        if (typeof data !== 'undefined') {
+            encodedData = [...new Uint8Array(CBOR.encode(data))];
+        }
+        const length_lo = encodedData.length & 255;
+        const length_hi = encodedData.length >> 8;
+        const group_lo = group & 255;
+        const group_hi = group >> 8;
+        const message = [op, _flags, length_hi, length_lo, group_hi, group_lo, this._seq, id, ...encodedData];
+        console.log(message.map(x => x.toString(16).padStart(2, '0')).join(' '));
+        await this._characteristic.writeValueWithoutResponse(Uint8Array.from(message));
+        this._seq = (this._seq + 1) % 256;
+    }
+    _notification(event) {
+        console.log('message received');
+        const message = new Uint8Array(event.target.value.buffer);
+        console.log(message);
+        this._buffer = new Uint8Array([...this._buffer, ...message]);
+        const messageLength = this._buffer[2] * 256 + this._buffer[3];
+        if (this._buffer.length < messageLength + 8) return;
+        this._processMessage(this._buffer.slice(0, messageLength + 8));
+        this._buffer = this._buffer.slice(messageLength + 8);
+    }
+    _processMessage(message) {
+        const [op, _flags, length_hi, length_lo, group_hi, group_lo, _seq, id] = message;
+        const data = CBOR.decode(message.slice(8).buffer);
+        const length = length_hi * 256 + length_lo;
+        const group = group_hi * 256 + group_lo;
+        if (group === MGMT_GROUP_ID_IMAGE && id === IMG_MGMT_ID_UPLOAD && data.rc === 0 && data.off) {
+            this._uploadOffset = data.off;
+            this._uploadNext();
+            return;
+        }
+        if (this._messageCallback) this._messageCallback({ op, group, id, data, length });
+    }
+    cmdReset() {
+        return this._sendMessage(MGMT_OP_WRITE, MGMT_GROUP_ID_OS, OS_MGMT_ID_RESET);
+    }
+    smpEcho(message) {
+        return this._sendMessage(MGMT_OP_WRITE, MGMT_GROUP_ID_OS, OS_MGMT_ID_ECHO, { d: message });
+    }
+    cmdImageState() {
+        return this._sendMessage(MGMT_OP_READ, MGMT_GROUP_ID_IMAGE, IMG_MGMT_ID_STATE);
+    }
+    cmdImageErase() {
+        return this._sendMessage(MGMT_OP_WRITE, MGMT_GROUP_ID_IMAGE, IMG_MGMT_ID_ERASE, {});
+    }
+    _hash(image) {
+        return crypto.subtle.digest('SHA-256', image);
+    }
+    async _uploadNext() {
+        if (this._uploadOffset >= this._uploadImage.byteLength) {
+            this._uploadIsInProgress = false;
+            this._imageUploadFinishedCallback();
+            return;
+        }
+
+        const nmpOverhead = 8;
+        const message = { data: new Uint8Array(), off: this._uploadOffset };
+        if (this._uploadOffset === 0) {
+            message.len = this._uploadImage.byteLength;
+            message.sha = new Uint8Array(await this._hash(this._uploadImage));
+        }
+        this._imageUploadProgressCallback({ percentage: Math.floor(this._uploadOffset / this._uploadImage.byteLength * 100) });
+
+        const length = this._mtu - CBOR.encode(message).byteLength - nmpOverhead;
+
+        message.data = new Uint8Array(this._uploadImage.slice(this._uploadOffset, this._uploadOffset + length));
+
+        this._uploadOffset += length;
+
+        this._sendMessage(MGMT_OP_WRITE, MGMT_GROUP_ID_IMAGE, IMG_MGMT_ID_UPLOAD, message);
+    }
+    async cmdUpload(image, slot = 0) {
+        if (this._uploadIsInProgress) {
+            this._logger.error('Upload is already in progress.');
+            return;
+        }
+        this._uploadIsInProgress = true;
+
+        this._uploadOffset = 0;
+        this._uploadImage = image;
+        this._uploadSlot = slot;
+
+        this._uploadNext();
+    }
+    async imageInfo(image) {
+        // https://interrupt.memfault.com/blog/mcuboot-overview#mcuboot-image-binaries
+
+        const headerSize = 32;
+
+        const info = {};
+        const view = new Uint8Array(image);
+
+        // check header length
+        if (view.length < headerSize) {
+            throw new Error('Invalid image (too short file)');
+        }
+
+        // check MAGIC bytes 0x96f3b83d
+        if (view[0] !== 0x3d || view[1] !== 0xb8 || view[2] !== 0xf3 || view[3] !== 0x96) {
+            throw new Error('Invalid image (wrong magic bytes)');
+        }
+
+        // check load address is 0x00000000
+        if (view[4] !== 0x00 || view[5] !== 0x00 || view[6] !== 0x00 || view[7] !== 0x00) {
+            throw new Error('Invalid image (wrong load address)');
+        }
+
+        // check header size is 32
+        if (view[8] !== headerSize || view[9] !== 0x00) {
+            throw new Error('Invalid image (wrong header size)');
+        }
+
+        // check protected TLV area size is 0
+        if (view[10] !== 0x00 || view[11] !== 0x00) {
+            throw new Error('Invalid image (wrong protected TLV area size)');
+        }
+
+        const imageSize = view[12] + view[13] * 2**8 + view[14] * 2**16 + view[15] * 2**24;
+        info.imageSize = imageSize;
+
+        // check image size is correct
+        if (view.length < imageSize + headerSize) {
+            throw new Error('Invalid image (wrong image size)');
+        }
+
+        // check flags is 0x00000000
+        if (view[16] !== 0x00 || view[17] !== 0x00 || view[18] !== 0x00 || view[19] !== 0x00) {
+            throw new Error('Invalid image (wrong flags)');
+        }
+
+        const version = `${view[20]}.${view[21]}.${view[22] + view[23] * 2**8}`;
+        info.version = version;
+
+        info.hash = [...new Uint8Array(await this._hash(image.slice(0, imageSize + 32)))].map(b => b.toString(16).padStart(2, '0')).join('');
+
+
+
+        return info;
+    }
+}
+
